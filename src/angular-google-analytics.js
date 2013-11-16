4928b2ee7fd279de1682572f87f9c825098140c0
Chris Esplin <christopher.esplin@gmail.com>
2013-11-16 09:17:13 -0700

    Implementing analytics.js support.

--- src/angular-google-analytics.js
+++ src/angular-google-analytics.js
@@ -9,8 +9,10 @@ angular.module('angular-google-analytics', [])
             accountId,
             trackPrefix = '',
             domainName,
-            filename = 'ga.js',
-            pageEvent = '$routeChangeSuccess';
+            analyticsJS = false,
+            pageEvent = '$routeChangeSuccess',
+            cookieConfig = 'auto',
+            ecommerce = false;
 
           this._logs = [];
 
@@ -23,6 +25,7 @@ angular.module('angular-google-analytics', [])
               trackRoutes = doTrack;
               return true;
           };
+
           this.trackPrefix = function(prefix) {
               trackPrefix = prefix;
               return true;
@@ -33,8 +36,8 @@ angular.module('angular-google-analytics', [])
             return true;
           };
 
-          this.setFilename = function(name) {
-            filename = name;
+          this.useAnalytics = function(val) {
+            analyticsJS = !!val;
             return true;
           };
 
@@ -43,10 +46,21 @@ angular.module('angular-google-analytics', [])
             return true;
           };
 
+          this.setCookieConfig = function (config) {
+            cookieConfig = config;
+            return true;
+          };
+
+          this.setECommerce = function (val) {
+            ecommerce = !!val;
+            return true;
+          };
+
         // public service
         this.$get = ['$document', '$rootScope', '$location', '$window', function($document, $rootScope, $location, $window) {
           // private methods
-          function _createScriptTag() {
+          function _createScriptTag() //noinspection JSValidateTypes
+          {
             // inject the google analytics tag
             if (!accountId) return;
             $window._gaq = [];
@@ -56,31 +70,67 @@ angular.module('angular-google-analytics', [])
             (function() {
               var document = $document[0];
               var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-              ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/' + filename;
+              ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
               var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
             })();
             created = true;
           }
+          function _createAnalyticsScriptTag() {
+            if (!accountId) {
+              return console.warn('No account id set for Analytics.js');
+            }
+
+            var args = [$window, $document, 'script', '//www.google-analytics.com/analytics.js', 'ga'],
+              scriptElement,
+              firstScript,
+              document = $document[0];
+            // inject the Analytics.js tag
+            $window['GoogleAnalyticsObject'] = 'ga';
+            $window.ga = $window.ga || function () {
+              (window.ga.q = window.ga.q || []).push([args]);
+            };
+            $window.ga.l = 1 * new Date();
+            scriptElement = document.createElement('script');
+            firstScript = document.getElementsByTagName('script')[0];
+            scriptElement.async = 1;
+            scriptElement.src = args[3];
+            firstScript.parentNode.insertBefore(scriptElement, firstScript);
+
+
+            $window.ga('create', accountId, cookieConfig);
+            if (trackRoutes) $window.ga('send', 'pageview');
+            if (ecommerce) $window.ga('require', 'ecommerce', 'ecommerce.js');
+
+          }
           this._log = function() {
             // for testing
+            //console.info('analytics log:', arguments);
             this._logs.push(arguments);
           };
           this._trackPage = function(url) {
-            if (trackRoutes && $window._gaq) {
+            if (trackRoutes && !analyticsJS && $window._gaq) {
               $window._gaq.push(['_trackPageview', trackPrefix + url]);
               this._log('_trackPageview', arguments);
+            } else if (trackRoutes && analyticsJS && $window.ga) {
+              $window.ga('send', 'pageview');
+              this._log('pageview', arguments);
             }
           };
           this._trackEvent = function(category, action, label, value) {
-            if ($window._gaq) {
+            if (!analyticsJS && $window._gaq) {
               $window._gaq.push(['_trackEvent', category, action, label, value]);
               this._log('trackEvent', arguments);
+            } else if ($window.ga) {
+              $window.ga('event', category, action, label, value);
+              this._log('event', arguments);
             }
+
           };
 
           /**
            * Add transaction
            * https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApiEcommerce#_gat.GA_Tracker_._addTrans
+           * https://developers.google.com/analytics/devguides/collection/analyticsjs/ecommerce#addTrans
            * @param transactionId
            * @param affiliation
            * @param total
@@ -92,15 +142,33 @@ angular.module('angular-google-analytics', [])
            * @private
            */
           this._addTrans = function (transactionId, affiliation, total, tax, shipping, city, state, country) {
-            if ($window._gaq) {
+            if (!analyticsJS && $window._gaq) {
               $window._gaq.push(['_addTrans', transactionId, affiliation, total, tax, shipping, city, state, country]);
               this._log('_addTrans', arguments);
+            } else if ($window.ga) {
+              if (!ecommerce) {
+                console.warn('ecommerce no set. Use AnalyticsProvider.setECommerce(true);');
+              } else {
+                $window.ga('ecommerce:addTransaction', {
+                  id: transactionId,
+                  affiliation: affiliation,
+                  revenue: total,
+                  tax: tax,
+                  shipping: shipping,
+                  city: city,
+                  state: state,
+                  country: country
+                });
+                this._log('ecommerce:addTransaction', arguments);
+              }
+
             }
           };
 
           /**
            * Add item to transaction
            * https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApiEcommerce#_gat.GA_Tracker_._addItem
+           * https://developers.google.com/analytics/devguides/collection/analyticsjs/ecommerce#addItem
            * @param transactionId
            * @param sku
            * @param name
@@ -110,26 +178,76 @@ angular.module('angular-google-analytics', [])
            * @private
            */
           this._addItem = function (transactionId, sku, name, category, price, quantity) {
-            if ($window._gaq) {
+            if (!analyticsJS && $window._gaq) {
               $window._gaq.push(['_addItem', transactionId, sku, name, category, price, quantity]);
               this._log('_addItem', arguments);
+            } else if ($window.ga) {
+              $window.ga('ecommerce:addItem', {
+                id: transactionId,
+                sku: sku,
+                name: name,
+                category: category,
+                price: price,
+                quantity: quantity
+              });
+              this._log('ecommerce:addItem', arguments);
             }
           };
 
           /**
            * Track transaction
            * https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApiEcommerce#_gat.GA_Tracker_._trackTrans
+           * https://developers.google.com/analytics/devguides/collection/analyticsjs/ecommerce#sendingData
            * @private
            */
           this._trackTrans = function () {
-            if ($window._gaq) {
+            if (!analyticsJS && $window._gaq) {
               $window._gaq.push(['_trackTrans']);
+              this._log('_trackTrans', arguments);
+            } else if ($window.ga) {
+              $window.ga('ecommerce:send');
+              this._log('ecommerce:send', arguments);
             }
-            this._log('_trackTrans', arguments);
+
           };
 
+          /**
+           * Clear transaction
+           * https://developers.google.com/analytics/devguides/collection/analyticsjs/ecommerce#clearingData
+           *
+           * @private
+           */
+          this._clearTrans = function () {
+            if ($window.ga) {
+              $window.ga('ecommerce:clear');
+              this._log('ecommerce:clear', arguments);
+            }
+          };
+
+          /**
+           * Send custom events
+           * https://developers.google.com/analytics/devguides/collection/analyticsjs/user-timings#implementation
+           * https://developers.google.com/analytics/devguides/collection/analyticsjs/social-interactions#implementation
+           *
+           * @param obj
+           * @private
+           */
+          this._send = function (obj) {
+            if ($window.ga) {
+              $window.ga('send', obj);
+              this._log('send', obj);
+            }
+          };
+
+
+
             // creates the ganalytics tracker
+          if (analyticsJS) {
+            _createAnalyticsScriptTag();
+          } else {
             _createScriptTag();
+          }
+
 
             var me = this;
 
@@ -140,6 +258,8 @@ angular.module('angular-google-analytics', [])
 
             return {
                 _logs: me._logs,
+                cookieConfig: cookieConfig,
+                ecommerce: ecommerce,
                 trackPage: function(url) {
                     // add a page event
                     me._trackPage(url);
@@ -156,6 +276,12 @@ angular.module('angular-google-analytics', [])
                 },
                 trackTrans: function () {
                     me._trackTrans();
+                },
+                clearTrans: function () {
+                  me._clearTrans();
+                },
+                send: function (obj) {
+                  me._send(obj);
                 }
             };
         }];
--- test/unit/angular-google-analytics.js
+++ test/unit/angular-google-analytics.js
@@ -78,22 +78,105 @@ describe('angular-google-analytics', function(){
   });
 
   describe('supports analytics.js', function() {
+    var cookieConfig = {
+      cookieDomain: 'foo.example.com',
+      cookieName: 'myNewName',
+      cookieExpires: 20000
+    };
+
     beforeEach(module(function(AnalyticsProvider) {
-      AnalyticsProvider.setFilename('analytics.js');
+      AnalyticsProvider.useAnalytics(true);
+      AnalyticsProvider.setCookieConfig(cookieConfig);
+      AnalyticsProvider.setECommerce(true);
     }));
 
     it('should inject the Analytics script', function() {
       inject(function(Analytics) {
-        expect(document.querySelectorAll("script[src='http://www.google-analytics.com/analytics.js']").length).toBe(1);
+        expect(document.querySelectorAll("script[src='//www.google-analytics.com/analytics.js']").length).toBe(1);
       });
     });
 
+    it('should support cookie config', function () {
+      inject(function(Analytics) {
+        expect(Analytics.ecommerce).toBe(true);
+      });
+    });
+
+    it('should allow transaction clearing', function () {
+      inject(function (Analytics) {
+        expect(Analytics._logs.length).toBe(0);
+        Analytics.clearTrans();
+        expect(Analytics._logs.length).toBe(1);
+      });
+    });
+
+    it('should allow sending custom events', function () {
+      inject(function (Analytics) {
+        var social = {
+          hitType: 'social',
+          socialNetwork: 'facebook',
+          socialAction: 'like',
+          socialTarget: 'http://mycoolpage.com',
+          page: '/my-new-page'
+        };
+
+        expect(Analytics._logs.length).toBe(0);
+        Analytics.send(social);
+        expect(Analytics._logs.length).toBe(1);
+        expect(Analytics._logs[0]).toEqual({
+          '0': 'send',
+          '1': social
+        });
+      });
+    });
+
+  });
+
+  describe('e-commerce transactions with analytics.js', function() {
+    beforeEach(module(function(AnalyticsProvider) {
+      AnalyticsProvider.useAnalytics(true);
+      AnalyticsProvider.setECommerce(true);
+    }));
+
+    it('should add transcation', function() {
+      inject(function(Analytics) {
+        expect(Analytics._logs.length).toBe(0);
+        Analytics.addTrans('1', '', '2.42', '0.42', '0', 'Amsterdam', '', 'Netherlands');
+        expect(Analytics._logs.length).toBe(1);
+        expect(Analytics._logs[0]['0']).toBe('ecommerce:addTransaction');
+      });
+    });
+
+    it('should add an item to transaction', function() {
+      inject(function(Analytics) {
+        expect(Analytics._logs.length).toBe(0);
+        Analytics.addItem('1', 'sku-1', 'Test product 1', 'Testing', '1', '1');
+        expect(Analytics._logs.length).toBe(1);
+        expect(Analytics._logs[0]['0']).toBe('ecommerce:addItem');
+      });
+    });
+
+    it('should track the transaction', function() {
+      inject(function(Analytics) {
+        expect(Analytics._logs.length).toBe(0);
+        Analytics.trackTrans();
+        expect(Analytics._logs.length).toBe(1);
+        expect(Analytics._logs[0]['0']).toBe('ecommerce:send');
+      });
+    });
   });
 
   describe('supports arbitrary page events', function() {
     beforeEach(module(function(AnalyticsProvider) {
       AnalyticsProvider.setPageEvent('$stateChangeSuccess');
     }));
+
+    it('should inject the Analytics script', function() {
+      inject(function(Analytics, $rootScope) {
+        $rootScope.$broadcast('$stateChangeSuccess');
+        expect(Analytics._logs.length).toBe(1);
+      });
+    });
 
     it('should inject the Analytics script', function() {
       inject(function(Analytics, $rootScope) {
