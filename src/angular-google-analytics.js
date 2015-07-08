0b3dd43684a1db0c167c08c791e5940c35b000a5
Jon Espen Kvisler <jonespen@gmail.com>
2015-07-08 13:39:58 +0200

    Added `ga-track-event-if` to directive
    Useful for user tracking opt-out etc. Solves GH-64

--- src/angular-google-analytics.js
+++ src/angular-google-analytics.js
@@ -913,6 +913,11 @@ angular.module('angular-google-analytics', [])
       link: function (scope, element, attrs) {
         var options = $parse(attrs.gaTrackEvent);
         element.bind('click', function () {
+          if(attrs.gaTrackEventIf){
+            if(!scope.$eval(attrs.gaTrackEventIf)){
+              return; // Cancel this event if we don't pass the ga-track-event-if condition
+            }
+          }
           if (options.length > 1) {
             Analytics.trackEvent.apply(Analytics, options(scope));
           }
