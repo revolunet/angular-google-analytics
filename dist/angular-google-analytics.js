0cd38a19d0eeef621335cc08794b444e764e09ce
Julien Bouquillon <julien@bouquillon.com>
2015-07-29 23:51:22 +0200

    bump 0.0.18

--- dist/angular-google-analytics.js
+++ dist/angular-google-analytics.js
@@ -1,6 +1,6 @@
 /**
  * Angular Google Analytics - Easy tracking for your AngularJS application
- * @version v0.0.17 - 2015-07-23
+ * @version v0.0.18 - 2015-07-29
  * @link http://github.com/revolunet/angular-google-analytics
  * @author Julien Bouquillon <julien@revolunet.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -249,10 +249,10 @@
           }
 
           // inject the google analytics tag
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){       // jshint ignore:line
-            (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),  // jshint ignore:line
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m); // jshint ignore:line
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');       // jshint ignore:line
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
           if (angular.isArray(accountId)) {
             accountId.forEach(function (trackerObj) {
